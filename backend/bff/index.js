d5a5e127c0176acd4e863d3b75bd790674ee5f79
oyas <poteto83@gmail.com>
2020-11-05 03:31:41 +0900

    Add mutation

--- backend/bff/index.js
+++ backend/bff/index.js
@@ -1,4 +1,4 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer, gql } = require("apollo-server")
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -19,7 +19,11 @@ const typeDefs = gql`
     books: [Book]
     time: String
   }
-`;
+
+  type Mutation {
+    addBook(title: String, author: String): Book
+  }
+`
 
 const books = [
   {
@@ -30,7 +34,7 @@ const books = [
     title: "City of Glass",
     author: "Paul Auster",
   },
-];
+]
 
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
@@ -39,13 +43,21 @@ const resolvers = {
     books: () => books,
     time: () => Date.now(),
   },
-};
+  Mutation: {
+    addBook: (_, { title, author }) => {
+      let book = { title, author }
+      books.push(book)
+      console.log("addBook() called: books =", books)
+      return book
+    },
+  },
+}
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers })
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+  console.log(`🚀  Server ready at ${url}`)
+})
--- frontend/web/src/pages/index.js
+++ frontend/web/src/pages/index.js
@@ -1,9 +1,7 @@
-import React from "react"
+import React, { useState } from "react"
 import { graphql, useStaticQuery } from "gatsby"
-import { useQuery } from '@apollo/client';
-import gql from 'graphql-tag';
+import { gql, useQuery, useMutation } from "@apollo/client"
 import Layout from "../components/layout"
-
 
 // This query is executed at build time by Gatsby.
 const GATSBY_QUERY = graphql`
@@ -23,8 +21,18 @@ const APOLLO_QUERY = gql`
   {
     books {
       title
-    },
+      author
+    }
     time
+  }
+`
+
+// This is mutation
+const ADD_BOOKS = gql`
+  mutation addBook($title: String!, $author: String!) {
+    addBook(title: $title, author: $author) {
+      title
+    }
   }
 `
 
@@ -37,25 +45,45 @@ const IndexPage = () => {
   // ----------------------
   // RUNTIME DATA FETCHING
   // ----------------------
-  const { loading, error, data: runtimeData } = useQuery(APOLLO_QUERY);
+  const { loading, error, data: runtimeData, refetch } = useQuery(APOLLO_QUERY)
+
+  // Mutation
+  const [addBook] = useMutation(ADD_BOOKS)
+
+  let input
 
   return (
     <Layout>
       <h1>Examples</h1>
       <h2>Build Time</h2>
-      <p>
-        {JSON.stringify(staticData)}
-      </p>
-      <p>
-        {new Date(parseInt(staticData.time)).toISOString()}
-      </p>
+      <p>{JSON.stringify(staticData)}</p>
+      <p>{new Date(parseInt(staticData.time)).toISOString()}</p>
       <h2>Runtime</h2>
-      <p>{JSON.stringify(runtimeData)}</p>
+      <pre>{JSON.stringify(runtimeData, null, 2)}</pre>
       <p>
         {loading && <span>Loading...</span>}
-        {error && <span style={{color: "red"}}>Error: ${error.message}</span>}
-        {runtimeData && parseInt(runtimeData.time) && new Date(parseInt(runtimeData.time)).toISOString()}
+        {error && <span style={{ color: "red" }}>Error: ${error.message}</span>}
+        {runtimeData &&
+          parseInt(runtimeData.time) &&
+          new Date(parseInt(runtimeData.time)).toISOString()}
       </p>
+      <h2>Mutation</h2>
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          addBook({ variables: { title: "Fox in Socks", author: input.value } })
+          input.value = ""
+          refetch()
+        }}
+      >
+        <input
+          ref={node => {
+            input = node
+          }}
+          placeholder="Dr. Seuss"
+        />
+        <button type="submit">Add book</button>
+      </form>
     </Layout>
   )
 }
