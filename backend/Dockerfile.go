55727a26ed69e14c6bd44bff7ea18e5637362387
oyas <poteto83@gmail.com>
2020-12-22 01:11:31 +0900

    Add dockerfile for bff

--- backend/Dockerfile.go
+++ backend/Dockerfile.go
@@ -1,17 +1,17 @@
-FROM golang:1.15.6-alpine as go-builder
+FROM golang:1.15.6-alpine as builder
 
 ARG SERVICE_NAME="greeter_server"
 
 # RUN apk update && apk add git
 
-ADD . /go/src
+COPY . /go/src
 
 WORKDIR /go/src/${SERVICE_NAME}
 
 RUN CGO_ENABLED=0 go build -v -o /go/bin/main
 
 
-FROM scratch as go-app
+FROM scratch as app
 
 COPY --from=go-builder /go/bin/main /app/main
 
