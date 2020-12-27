09346c0759757939ec81fe02a91f3849a7c02dc7
oyas <poteto83@gmail.com>
2020-12-27 23:01:17 +0900

    Add nginx

--- frontend/web/src/apollo/client.js
+++ frontend/web/src/apollo/client.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'http://localhost:4000/graphql',
+    uri: '/graphql',
     fetch,
   }),
   cache: new InMemoryCache()
