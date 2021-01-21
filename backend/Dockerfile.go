bb215f9ea82af471cfe14ec848f1fd8e8e671db5
oyas <poteto83@gmail.com>
2021-01-22 01:51:33 +0900

    Fix dockerfile multistage order

--- backend/Dockerfile.go
+++ backend/Dockerfile.go
@@ -10,13 +10,13 @@ WORKDIR /go/src/${SERVICE_NAME}
 RUN CGO_ENABLED=0 go build -v -o /go/bin/main
 
 
-FROM builder as test
-
-RUN CGO_ENABLED=0 go test -v ./...
-
-
 FROM scratch as app
 
 COPY --from=builder /go/bin/main /app/main
 
 ENTRYPOINT ["/app/main"]
+
+
+FROM builder as test
+
+RUN CGO_ENABLED=0 go test -v ./...
