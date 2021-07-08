63808a0168f9222304e6b8e07709849917fe7beb
oyas <poteto83@gmail.com>
2021-07-09 00:36:03 +0900

    Add .env and `NEXT_PUBLIC_URL_BFF`

--- frontend/web/lib/apollo/client.js
+++ frontend/web/lib/apollo/client.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const URL_BFF = process.env.API_URL_BFF_RUNTIME || "http://localhost:4000";
+const URL_BFF = process.env.API_URL_BFF_RUNTIME || process.env.NEXT_PUBLIC_URL_BFF || "";
 
 export const client = new ApolloClient({
   uri: `${URL_BFF}/graphql`,
