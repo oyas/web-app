16dff2c5c5166e66eb4ebbf0d841b12bedad8183
oyas <poteto83@gmail.com>
2021-08-19 03:26:54 +0900

    Generete RSA keys before test

--- backend/auth/keys/generate.sh
+++ backend/auth/keys/generate.sh
@@ -2,5 +2,20 @@
 
 cd `dirname $0`
 
-openssl genrsa -out app.rsa 2048
-openssl rsa -in app.rsa -pubout > app.rsa.pub
+RSA_NAME=app.rsa
+RSA_PUB_NAME=app.rsa.pub
+MOUNT_DIR=/tmp/keys
+RSA_PATH=$MOUNT_DIR/$RSA_NAME
+RSA_PUB_PATH=$MOUNT_DIR/$RSA_PUB_NAME
+
+touch $RSA_NAME
+touch $RSA_PUB_NAME
+
+CMD="
+  apt-get update &&
+  apt-get install -y openssl &&
+  openssl genrsa -out $RSA_PATH 2048 &&
+  openssl rsa -in $RSA_PATH -pubout -out $RSA_PUB_PATH
+"
+
+docker run --rm -v $(pwd):$MOUNT_DIR ubuntu bash -c "$CMD"
