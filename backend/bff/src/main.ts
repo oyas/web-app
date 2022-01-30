7863f9a3d62d815e50bb39c5d8b87caf1b8bd141
oyas <poteto83@gmail.com>
2022-01-31 01:34:11 +0900

    test of k8s deployment

--- backend/bff/src/main.ts
+++ backend/bff/src/main.ts
@@ -1,6 +1,7 @@
 import { setUpTracer } from "./tracer";
 import { makeGrpcClients } from "./grpc-clients";
 import { makeServer } from "./server";
+import { setTimeout } from "timers/promises";
 
 const SCHEMA_GRAPHQL_PATH = __dirname + "/../schema.graphql";
 
@@ -8,19 +9,27 @@ const API_URL_ARTICLES = process.env.API_URL_ARTICLES || "localhost:50052";
 const API_URL_AUTH = process.env.API_URL_AUTH || "localhost:50054";
 const APP_JAEGER_URL = process.env.APP_JAEGER_URL || "http://localhost:14268";
 
-// set up OpenTelemetry Tracer
-setUpTracer(APP_JAEGER_URL, "bff");
+async function main(){
+    // set up OpenTelemetry Tracer
+    setUpTracer(APP_JAEGER_URL, "bff");
 
-// set up gRPC clients
-const clients = makeGrpcClients({
-  articles: API_URL_ARTICLES,
-  auth: API_URL_AUTH,
-});
+    // set up gRPC clients
+    const clients = makeGrpcClients({
+      articles: API_URL_ARTICLES,
+      auth: API_URL_AUTH,
+    });
 
-// set up GraphQL server
-const server = makeServer(SCHEMA_GRAPHQL_PATH, clients);
+    // set up GraphQL server
+    const server = makeServer(SCHEMA_GRAPHQL_PATH, clients);
 
-// The `listen` method launches a web server.
-server.listen().then(({ url }: { url: String }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+    // sleep for startup test
+    console.log("sleep 30 sec...");
+    await setTimeout(30000);
+
+    // The `listen` method launches a web server.
+    server.listen().then(({ url }: { url: String }) => {
+      console.log(`🚀  Server ready at ${url}`);
+    });
+}
+
+main();
